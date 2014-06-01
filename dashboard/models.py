d356634c4c6a10b3c2bb6d927f88aadb786fdb2d
Arman Tokanov <armatoka@gmail.com>
2014-05-31 22:40:15 -0400

    Created models

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,4 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django_countries.fields import CountryField
 
 # Create your models here.
+
+class Candidates(models.Model):
+	first_name = models.CharField(max_length=200)
+	last_name = models.CharField(max_length=200)
+	birth_date = models.DateField()
+	nationality = CountryField()
+	LEVELS = (
+		(1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'),
+		(6, '6'), (7, '7'), (8, '8'), (9, '9'),	(10, '10'),
+	)
+	level_of_english = models.CharField(max_length=2, choices=LEVELS, default=1)
+	note = models.TextField()
+	resume = models.FileField(upload_to='resumes')
