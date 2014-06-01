55f7ae604458b9b5d2c49896578815068f184dfe
Arman Tokanov <armatoka@gmail.com>
2014-05-31 20:09:44 -0400

    created login handling

--- CandidateDatabase/urls.py
+++ CandidateDatabase/urls.py
@@ -9,4 +9,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/$', 'django.contrib.auth.views.login', name="my_login"),
 )
